ec0b8d8235d21890ff23fc56d757e681390d312d
Hayley Guillou <hayleyguillou@gmail.com>
2014-11-24 10:35:21 -0600

    Demo today widget - Used space graph

--- Used-Space/Used Space/TodayViewController.swift
+++ Used-Space/Used Space/TodayViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
+
     
     @IBOutlet weak var percentLabel: UILabel!
     @IBOutlet weak var barView: UIProgressView!
@@ -19,12 +20,12 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     var fileSystemSize: UInt64 = 0
     var freeSize: UInt64 = 0
     var usedSize: UInt64 = 0
-    //var usedRate: Double = 0
+    var usedRate: Double = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initializeSizes()
         updateInterface()
-        percentLabel.text = "ran"
         // Do any additional setup after loading the view from its nib.
     }
     
@@ -33,24 +34,35 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // Dispose of any resources that can be recreated.
     }
     
+    func initializeSizes() {
+        updateSizes()
+        usedRate = Double(self.usedSize) / Double(self.fileSystemSize)
+        setUsedRate(usedRate)
+    }
+    
     func updateInterface() {
-        var rate: Double = self.usedRate; // retrieve the cached value
-        self.percentLabel.text = String(format: "%.1d%%", (rate*100))
+        var rate: Double = self.getUsedRate(); // retrieve the cached value
+        self.percentLabel.text = String(format: "%0.1f%%", rate*100)
         self.barView.progress = Float(rate);
     }
     
     func updateSizes() {
         // Retrieve the attributes from NSFileManager
         var dict: NSDictionary = NSFileManager.defaultManager().attributesOfFileSystemForPath(NSHomeDirectory(), error: nil)!
-        
+    
         // Set the values
         fileSystemSize = UInt64(dict.valueForKey(NSFileSystemSize) as Int)
         freeSize       = UInt64(dict.valueForKey(NSFileSystemFreeSize) as Int)
         usedSize       = self.fileSystemSize - self.freeSize;
     }
     
-    var usedRate() -> (Double) {
-    return NSUserDefaults.standardUserDefaults().valueForKey(RATE_KEY) as Double
+    func getUsedRate() -> Double {
+        var usedRate: AnyObject? = NSUserDefaults.standardUserDefaults().valueForKey(RATE_KEY)
+        if (usedRate != nil) {
+            return usedRate as Double
+        } else {
+            return self.usedRate
+        }
     }
     
     func setUsedRate(usedRate: Double)
@@ -61,27 +73,15 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     }
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
-        // Perform any setup necessary in order to update the view.
-        
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-        
         updateSizes()
         var newRate = Double(self.usedSize) / Double(self.fileSystemSize)
-        if (newRate - usedRate()) < 0.0001 {
+        if (newRate - self.usedRate) < 0.0001 {
             completionHandler(NCUpdateResult.NoData)
         } else {
             setUsedRate(newRate)
             updateInterface()
             completionHandler(NCUpdateResult.NewData)
         }
-    }
-    
-    func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
-        var margins: UIEdgeInsets = defaultMarginInsets
-        margins.bottom = 10.0
-        return margins
     }
     
 }
